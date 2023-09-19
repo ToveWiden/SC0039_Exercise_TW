dacff57c12a50857745cae3a3de83a55dcc67c23
Tove Widén <wtove@chalmers.se>
2023-09-19 16:30:29 +0200

    added comments

--- assignment1.py
+++ assignment1.py
@@ -1,28 +1,20 @@
-#open or read file
-#create a new empty column
-#for loop 
-# create variable takes position - position, put number in new column
-
-
-#import csv
-
-#list comprehension chapter skapar lists med for loop 
 import csv
 
+#open and read the csv file 
 with open('brca_cnvs_tcga-1.csv', newline='') as f:
     reader = csv.reader(f)
-    header = next(reader)
-    header.append("seq_length")
-    row_list = []
-    for row in reader:
-        x=int(row[2])
-        y=int(row[3])
-        z=(y-x)
-        r = row
-        r.append(z)
-        row_list.append(r)
+    header = next(reader) #assigns the first row in the file to the header variable
+    header.append("seq_length") #adds seq_length to header
+    row_list = [] #creates an empty list 
+    for row in reader: #iterates over the rows in the file, except the first
+        x=int(row[2]) #assigns the third value of the row to x and makes it an integer
+        y=int(row[3]) #assigns the fourth value of the row to y and makes it an integer
+        z=(y-x) #calculates the sequence length
+        r = row #assigns the list in each row to variable r
+        r.append(z) #adds the seq length to the list r
+        row_list.append(r) #adds the list r to row_list, creating a list of lists
     
-with open('new_file.csv', 'w', newline='') as nf:
+with open('new_file.csv', 'w', newline='') as nf: #creates a new csv file
     writer = csv.writer(nf)
-    writer.writerow(header)
-    writer.writerows(row_list)
+    writer.writerow(header) #adds the header
+    writer.writerows(row_list) #adds rows from row_list
